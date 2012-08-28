04f6c8afc929528213f9bcc686416609aef13519
Epicgrim <nathanlil13@me.com>
2012-08-28 04:05:34 -0400

    fixing comment

--- helpers.lua
+++ helpers.lua
@@ -2,7 +2,7 @@ local PI = select(2, ...)
 
 -- ## Helper Functions
 
--- ### `has_aura( string, string, string )
+-- ### `has_aura( string, string, string )`
 -- Returns true when the given unit has the given aura. Filter is a string
 -- describing attributes of the aura. For a complete list of valid filters
 -- look up the documentation of `UnitAura()`.
