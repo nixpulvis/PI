a08fbc229f50959405050c23ae095f7073084507
Nathan Lilienthal <nathanlil13@me.com>
2012-12-08 02:16:09 -0500

    more info on /pi commands

--- cli.lua
+++ cli.lua
@@ -1,33 +1,51 @@
 local PI = select(2, ...)
 
+local function print_help()
+  print("PI (perfect interrupt)")
+  print("  help : Display this message.")
+  print("  move : Toggle moving the blocker frame.")
+  print("  cast [SPELL] : Watch for given spellcast.")
+  print("  aura [SPELL] : Watch for given aura.")
+  print("  reset : Reset PI's watching.")
+  print(" ")
+  print("  made with love by Nathan Lilienthal (Epicgrim)")
+end
+
 SLASH_PI1 = '/pi'
 function SlashCmdList.PI( msg, editbox )
 
   -- no args
   if msg == "" then
-    print("PI (Perfect Interrupt)")
+    print_help()
   else
 
-    -- ### `/pi move` or `/pi resize`
-    if msg == "move" or msg == "resize" then
-      print("moving")
+    -- ### `/pi move`
+    if msg == "move" then
+      if PI.blocker:IsMovable() then
+        print("PI is now locked")
+      else
+        print("PI is now movable")
+      end
       PI.blocker:toggle_movable()
     end
 
     -- ### `/pi cast _player_ _spell_`
     if string.find(msg, "cast") then
       local unit, spell = strsplit(" ", string.gsub(msg, "cast ", ""), 2)
+      print("PI is now watching "..unit.." for "..spell)
       PI.blocker:pass_on_cast(unit, spell)
     end
 
     -- ### `/pi aura _player_ _spell_ _filter_`
     if string.find(msg, "aura") then
       local unit, spell, filter = strsplit(" ", string.gsub(msg, "aura ", ""), 3)
+      print("PI is now watching "..unit.." for "..spell.."["..filter.."]")
       PI.blocker:pass_on_aura(unit, spell, filter)
     end
 
     -- ### `/pi reset`
     if msg == "reset" then
+      print("PI is not watching anything now")
       PI:reset_detection()
     end
 
